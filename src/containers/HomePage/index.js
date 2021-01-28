1e3910f455af622c042431daa8424daf5b702086
sellercentral735 <59581071+sellercentral735@users.noreply.github.com>
2021-01-28 16:03:32 +0530

    Update index.js

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import Layout from '../../components/Layout';
 
 /**
@@ -9,7 +11,17 @@ import Layout from '../../components/Layout';
 const HomePage = (props) => {
   return(
     <Layout>
-      Home Page
+      <Carousel>
+         <div className="legend1">
+            <img src="https://rukminim1.flixcart.com/flap/1688/280/image/9684c5bba6b14e7f.jpg?q=50" alt="" />
+         </div>
+         <div className="legend2">
+            <img src="https://rukminim1.flixcart.com/flap/1688/280/image/a61e29c24c8934dd.jpg?q=50" alt="" />
+          </div>
+          <div className="legend3">
+            <img src="https://rukminim1.flixcart.com/flap/1688/280/image/90cdb794821102c8.jpg?q=50" alt="" />
+          </div>
+       </Carousel>
     </Layout>
    )
 
